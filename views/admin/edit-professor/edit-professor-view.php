d20c9733c1bd79f8f2d287d088ff9c85fda34d9b
leoaltissimoneto@gmail.com <leoaltissimoneto@gmail.com>
2019-03-06 01:11:13 -0300

    tabela para adicionar professores

--- views/admin/edit-professor/edit-professor-view.php
+++ views/admin/edit-professor/edit-professor-view.php
@@ -33,5 +33,30 @@ if( $parametros ) {
 } else {
 ?>
 
+<section class="dash-card">
+    <div class="header-container">
+        <h1><span class="first-letter">P</span>rofessores</h1>
+    </div>
+    <div>
+        <form method="POST">
+        <div class="col-12 form-separator">
+            <input class="col-1 prof-input-text" type="text" name="titulacao" placeholder="Titulação" />
+            <input class="col-9 prof-input-text" type="text" name="nome" placeholder="Nome" />
+        </div>        
+        <div class="col-12 form-separator">
+            <input class="col-4 prof-input-text" type="tel" name="tel" placeholder="tel" />
+            <input class="col-6 prof-input-text" type="email" name="email" placeholder="email" />
+        </div>
+        <div class="col-12 form-separator">
+            <input class="col-4 prof-input-text" type="text" name="lattes" placeholder="Link Lattes" />
+            <input class="col-6 prof-input-text" type="text" name="facebook" placeholder="Facebook" />
+        </div>
+        <div class="col-12 form-separator">
+            <textarea class="col-12 prof-input-textarea" name="apresentacao" placeholder="Apresentacao" rows="4"></textarea>
+        </div>
+            <input class="form-submit" type="submit">
+        </form>
+    </div>
+</section>
 
 <?php } ?>
